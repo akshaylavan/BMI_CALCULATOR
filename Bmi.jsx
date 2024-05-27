5f4bf302b78a19344e4d551d23c3759a449f51cc
Akshay Lavan <133519950+akshaylavan@users.noreply.github.com>
2024-05-27 16:30:38 +0530

    Added Clear Button

--- Bmi.jsx
+++ Bmi.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import dietIcon from "./BMIphoto/diet.jpg";
 import { useState } from "react";
+
 export const Bmi = () => {
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
@@ -28,14 +29,23 @@ export const Bmi = () => {
     } else {
       setBmi(null);
       setBmiStatus("");
-      setErrorMessage("Please Enter an Valid Numbers");
+      setErrorMessage("Please Enter a Valid Number");
     }
   };
+
+  const clear = () => {
+    setHeight("");
+    setWeight("");
+    setBmi(null);
+    setBmiStatus("");
+    setErrorMessage("");
+  };
+
   return (
     <>
       <section className="text-center w-screen h-screen flex justify-center items-center bg-blue-50">
-        <div className=" border border-blue-500 hover:border-blue-600 hover:border-4 shadow-lg  rounded-lg  w-auto h-auto pr-40 pl-40 bg-white">
-          <h1 className="font-post text-4xl text-blue-700 flex justify-center mb-3 mt-5 items-center select-none ">
+        <div className="border border-blue-500 hover:border-blue-600 hover:border-4 shadow-lg rounded-lg w-auto h-auto pr-40 pl-40 bg-white">
+          <h1 className="font-post text-4xl text-blue-700 flex justify-center mb-3 mt-5 items-center select-none">
             BMI Calculator
           </h1>
           <img src={dietIcon} alt="" className="w-80 mb-4 select-none" />
@@ -47,14 +57,12 @@ export const Bmi = () => {
           <div>
             <label
               htmlFor="height"
-              className="block  font-ubuntu font-semibold text-2xl mb-3 select-none"
+              className="block font-ubuntu font-semibold text-2xl mb-3 select-none"
             >
-              {" "}
               Height (CM)
             </label>
             <input
               type="text"
-              name=""
               id="height"
               placeholder="Enter Height"
               className="text-2xl mb-4 p-2 focus:outline-blue-700 border-2 rounded-md border-blue-500"
@@ -64,13 +72,12 @@ export const Bmi = () => {
             <div>
               <label
                 htmlFor="weight"
-                className="block font-ubuntu font-semibold text-2xl mb-3 select-none "
+                className="block font-ubuntu font-semibold text-2xl mb-3 select-none"
               >
-                Weigth(Kg)
+                Weight (Kg)
               </label>
               <input
                 type="text"
-                name=""
                 id="weight"
                 placeholder="Enter Weight"
                 className="text-2xl mb-4 p-2 focus:outline-blue-700 border-2 rounded-md border-blue-500"
@@ -81,17 +88,23 @@ export const Bmi = () => {
           </div>
           <div>
             <button
-              className="hover:bg-blue-900 bg-blue-500 text-white p-3 rounded-lg text-2xl m-3 font-nano font-medium "
+              className="hover:bg-blue-900 bg-blue-500 text-white p-3 rounded-lg text-2xl m-3 font-nano font-medium"
               onClick={calculateBmi}
             >
-              Calulate BMI
+              Calculate BMI
+            </button>
+            <button
+              className="hover:bg-red-700 bg-red-400 text-white p-3 rounded-lg text-2xl m-3 font-nano font-medium"
+              onClick={clear}
+            >
+              Clear
             </button>
             {bmi !== null && (
               <div className="text-3xl m-2 font-bold font-nano">
                 <p className="m-2 mt-6 mb-6 text-blue-600">
-                  Your BMI is : {bmi}
+                  Your BMI is: {bmi}
                 </p>
-                <p className="m-2 mb-7 font-nano"> Status: {status}</p>
+                <p className="m-2 mb-7 font-nano">Status: {status}</p>
               </div>
             )}
           </div>
